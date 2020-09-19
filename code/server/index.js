4f80078a97d8fbd8f07fdac7b1fc0590a4b85dd3
urvishvasani <ur207vasani@gmail.com>
2020-09-19 19:21:23 -0400

    updated front end and server code to use socket.io for communication

--- code/server/index.js
+++ code/server/index.js
@@ -3,9 +3,15 @@ const app = express()
 const path = require('path')
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-
 const PORT = 5000;
 
 app.use(express.static(path.resolve(__dirname,'../public')));
+
+io.on('connection', socket => {
+    socket.on('image',(data)=>{
+        socket.emit('new', data);
+    })
+});
+
 
 server.listen(PORT, ()=> console.log('server is listening on port ' + PORT))
